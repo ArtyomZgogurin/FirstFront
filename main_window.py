ab4bd6308affef2c29c590684f3c460953287526
ArtyomZgogurin <73875029+ArtyomZgogurin@users.noreply.github.com>
2020-11-21 16:38:27 +0500

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -5,6 +5,8 @@ from tkinter import filedialog
 from PIL import ImageTk,Image
 import xml.etree.ElementTree as ET
 import os, fnmatch
+import time
+
 
 root = Tk()
 root['bg'] = 'white'
@@ -12,19 +14,20 @@ root.title('Узнать данные с Билдов')
 root.geometry('750x450')
 root.resizable(width=True, height=True)
 
-path = "C:\\testttttt"
-files = []
-file_names = []
+comment = ''                                      #КОММЕНТ ДЛЯ АРТЕМА  
+start_time = time.time()                          #засекаем время
+path = "C:\\testttttt"                            #задаем корневаю папку (тут будет инфа от GUI)
+pattern_for_file = '*.btl'                        #задаем расширение файлов, которые будем парсить
 
 #фрейм для поля парсера
 frame_par = Frame(root, bg='snow3', bd=5)
-frame_par.place(relx=0.02, rely=0.15, relwidth=0.45, relheight=0.25)
+frame_par.place(relx=0.02, rely=0.10, relwidth=0.45, relheight=0.25)
 #фрейм для поля коммента
 frame_com = Frame(root, bg='snow3', bd=5)
-frame_com.place(relx=0.02, rely=0.55, relwidth=0.45, relheight=0.25)
+frame_com.place(relx=0.02, rely=0.35, relwidth=0.45, relheight=0.25)
 #фрейм для поля пути
-frame_way = Frame(root, bg='snow3', bd=5)
-frame_way.place(relx=0.53, rely=0.15, relwidth=0.45, relheight=0.25)
+frame_build = Frame(root, bg='snow3', bd=5)
+frame_build.place(relx=0.02, rely=0.6, relwidth=0.45, relheight=0.25)
 #фрейм для canvasa
 frame_pic = Frame(root, bg='white', bd=5)
 frame_pic.place(relx=0.68, rely=0.88, relwidth=0.45, relheight=0.25)
@@ -34,129 +37,122 @@ image1 = ImageTk.PhotoImage(imageEm)
 
 
 #info
-info = Label(frame_par, text='Какой билд будем парсить?', bg='snow3', font=30)
+info = Label(frame_par, text='Ревизия билда', bg='snow3', font=30)
 info.pack()
-info = Label(frame_com, text='Какие изменения подъехали?', bg='snow3', font=30)
+info = Label(frame_com, text='Комментарий', bg='snow3', font=30)
 info.pack()
-info = Label(frame_way, text='Путь до отчётов', bg='snow3', font=30)
+info = Label(frame_build, text='Путь до отчётов', bg='snow3', font=30)
 info.pack()
 info = Label(frame_pic, image=image1, bg='snow3', font=30)
 info.pack()
 #устанавливаем привязку к вводным данным
-input_parser=StringVar()
-input_comment=StringVar()
-input_way=StringVar()
-#ввод для парсинга
-buildPar = Entry(frame_par, bg='white', font=30, textvariable=input_parser )
-buildPar.pack()
-#ввод для коммента
-commentBuild=Entry(frame_com, bg='white', font=30, textvariable=input_comment )
-commentBuild.pack()
-#ввод пути
-wayBuild=Entry(frame_way, bg='white', font=30, textvariable=input_way)
-wayBuild.pack()
 
-def xml_parse(xml_file, tag):
-    result = {}
-    results = xml_file.findall(tag)
+path_roots=StringVar()
+input_comment=StringVar()
+input_build=StringVar()
+def xml_parse(xml_file):
+    result = []
+    #result = {}                                    #словарь на случай словаря)
+    results = xml_file.findall('results')           #находим тег <results>
     j = -1
     t = True
-    for i in results:                          
+    for i in results:                      
         while t == True:  
             try:
-                j += 1                                    #парсим 35 тегов    
+                j += 1                                      
                 temp = i[j].text.strip()            #закидваем значение, удаляя пробелы
-                name = i[j].tag              
-                result[name] = temp                 #добавляем "тег = значение" в словарь
-            except:
-                AttributeError
-                FileNotFoundError
-                try:                                #если вдруг в теге есть аттрибуты, то добавляем их
-                    j += 1                                      
-                    temp = i[j].attrib            
-                    name = i[j].tag              
-                    result[name] = temp             #добавляем "тег = аттрибут" в словарь    
-                except:
-                    AttributeError
-                    FileNotFoundError
-                    t = False
-                    pass
+                #name = i[j].tag                    #берем имя каждого тега на всякий случай
+                result.append(temp)                 #ДОБАВЛЯЕМ КАЖДОЕ ЗНАЧЕНИЕ В СПИСОК result[]
+            except IndexError:
+                t = False
+                pass
                 
-
-    for i in result:
-        print(i, '=', result[i])              #выводим всё, что распарсили
+    #for i in result:
+        print(result)              #выводим всё, что распарсили (А КОНКРЕТНЕЕ СПИСОК СО ВСЕМИ ЗНАЧЕНИЯМИ ПО ПОРЯДКУ)
     print('Файл пропарсен')
 
 def get_map_name(file):                            #вытаскиваем название карты из имени файла
-    map_name = ''
-    index = file.index('_wotreplay')
-    temp = file[index-1]
-    while not temp.isdigit():                                       
-        temp = file[index-1] 
-        index -= 1            
-        map_name += temp
-    while temp.isdigit():
-        temp = file[index-1]
-        index -= 1       
-        map_name += temp
-    map_name = map_name[::-1]
-    map_name = map_name[1:]   
-    return map_name
+    index = file.index('.wotreplay')
+    temp = file[0:index]
+    return temp
 
+def get_stand_name(folder):
+    stand_name = folder[0:-19]
+    return stand_name
 
-def way_input():
+def main_parsing():
+    print('введите')
+    path_root = path_roots.get()
+    build_number = input_build.get()
+    comment = input_comment.get()
+    files = []                                      #список файлов с полным путем
+    file_names = []                                 #список названий файлов                                  
+    list_of_folders = os.listdir(path_root)         #список имен папок 
+    count_d = 0                                     #каунтер папок
+    count_f = 0                                     #каунтер файлов
+    print('Список папок в корне:')
+    for dir in list_of_folders:                                         #выводим список папок в корне с нумерацией
+        count_d += 1
+        print(str(count_d) + '. ' + dir)
+
     try:
-        print('Введите название стенда:')
-        dir = input_way.get()                                   #вводим название стенда (папки)
-        listOfFiles = os.listdir(path + '\\' + dir)     #добавляем его в путь
-        print('Введите расширение файлов для парсинга:')
-        pattern_for_file = '*.' + input()               #вводим расширение файла для парсинга(для демо, захардкодим расширение, когда доделаем)
+        for dir in list_of_folders:                                     #идем по каждой папке в корне
+            new_path = path_root + '\\' + dir                                             
+            listOfFiles = os.listdir(new_path)     
+            print('Добавлена директория для парсинга ' + new_path)             
 
-        for entry in listOfFiles:
-            if fnmatch.fnmatch(entry, pattern_for_file):
-                print('Добавлен файл для парсинга ' + entry)    
-                files.append(path + '\\' + dir + '\\' + entry)   #создаем список файлов с полным путем
-                file_names.append(entry)                        #создаем отдельно список названий файлов
-        if files == []:                                         #если список по итогу пустой, то файлов с нужным расширением у нас нет
-            print('Файлов с таким расширением не найдено')
-        else:
-            print('Введите название тега для парсинга:')        #вводим тег, внутри которого мы будем доставать инфу
-            input_tag = input()
+            for entry in listOfFiles:                                      #идем по каждому файлу в папке
+                if fnmatch.fnmatch(entry, pattern_for_file):
+                    print('Добавлен файл для парсинга ' + dir + '\\' + entry)    
+                    files.append(new_path + '\\' + entry)           #создаем список файлов с полным путем
+                    file_names.append(entry)                        #создаем отдельно список названий файлов
+                    get_table_name = get_stand_name(dir) + get_map_name(entry)         #СОЗДАЕМ TABLE_NAME
+                    print('Название таблицы: ' + get_table_name)            
     
-        for i in files:                                         
-            try:
-                xml_file = ET.parse(i)
-                print('Парсим файл', i)
-                print('Название карты: ' + get_map_name(i))     #выводим название карты для каждого файла
-                xml_parse(xml_file,input_tag)                   
-            except:
-                FileNotFoundError
-                xml_file = ET.parse(i)                          #except для файлов другой структуры(если нет в имени файла название карты)
-                xml_parse(xml_file,input_tag)                   
-                pass  
-    except:
-        FileNotFoundError
-        print('Такой дериктории не найдено')
-def parser():
-  print('скоро мы напишем парсер')
-  second_window.second_window_par()
-def comment():
-  print('скоро мы напишем комментарий')
-  messagebox.showinfo('Комментарий отправлен',input_comment.get())
-def way():
-  print('скоро мы научимся искать')
-  file = filedialog.askopenfilename()
 
+            if files == []:                                         
+                print('Файлов в папке', dir, 'не найдено')
+    
+            for i in files:                                #парсим все каждый файл в папке         
+                try:
+                    xml_file = ET.parse(i)
+                    print('Парсим файл', i)                    
+                    xml_parse(xml_file)
+                    count_f += 1
+                    
+                except FileNotFoundError:           
+                    print('Ошибка в файле ' + xml_file)         #если название файла корявое, то скипаем его         
+                    pass
+            files = []                                          #обнуляем список файлов
+
+    except FileNotFoundError:    
+        print('Такой дериктории не найдено')
+
+    print('Пропарсено папок: ' + str(count_d))
+    print('Пропарсено файлов: ' + str(count_f))
+    print("за %s seconds" % (time.time() - start_time))
+
+
+
+#ввод для парсинга
+buildPar = Entry(frame_par, bg='white', font=30, textvariable=input_build)
+buildPar.pack()
+#ввод для коммента
+commentBuild=Entry(frame_com, bg='white', font=30, textvariable=input_comment)
+commentBuild.pack()
+#ввод пути
+wayBuild=Entry(frame_build, bg='white', font=30, textvariable=path_roots)
+wayBuild.pack()
 
 
 #кнопка парсера
-btnPar = Button(frame_par, text='Парсить', command=parser)
+btnPar = Button(frame_build, text='Парсить', command=main_parsing)
 btnPar.pack()
 #кнопка комментария
-btnCom = Button(frame_com, text='Добавить комментарий', command=comment)
-btnCom.pack()
+#btnCom = Button(frame_com, text='Добавить комментарий', command=comment)
+#btnCom.pack()
 #кнопка пути
-btnWay = Button(frame_way, text='Указать путь', command=way_input)
-btnWay.pack()
+#btnWay = Button(frame_build, text='Указать путь', command=build_input)
+#btnWay.pack()
 
 root.mainloop()
